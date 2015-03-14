46ae512668e3f98ece4f1060acc4ca75e72e4d62
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-14 13:20:27 +0530

    v1.0 | Customer will get room if available according to the max Number of people. new methods are written such as initiateRooms(). getRoom() updated. Used mazic number

--- tests/com/hotels/ReceptionTest.java
+++ tests/com/hotels/ReceptionTest.java
@@ -6,22 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by kaustavc on 3/14/2015.
+ * Reception test file for a hotel transaction
  */
 public class ReceptionTest {
     @Test
-    public void testManagerShouldEnlistAValidCustomerDetails() {
+    public void testCustomerShouldGetRoomIfAvailable() {
         List<Room> rooms = new ArrayList<Room>();
         rooms.add(new Room(2, 3, 101, 2));
 
         Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
-        Manager manager = hotel.addManager("Santosh");
+        hotel.addManager("Santosh");
         Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
 
         Room rm = hotel.getRoom(sukhvindar.getDetails(), 3);
         assertNotNull(rm);
     }
 
+    @Test
+    public void testCustomerShouldGetRoomIfNotAvailable() {
+        List<Room> rooms = new ArrayList<Room>();
+        rooms.add(new Room(2, 3, 101, 2));
+
+        Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
+        hotel.addManager("Santosh");
+        Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
+
+        Room rm = hotel.getRoom(sukhvindar.getDetails(), 4);
+        assertNull(rm);
+    }
+
+    @Test
+    public void testCustomerShouldGetRoomIfMultipleRoomAvailableFor3People() {
+        List<Room> rooms = new ArrayList<Room>();
+        rooms.add(new Room(2, 3, 201, 2));
+        rooms.add(new Room(2, 3, 202, 2));
+        rooms.add(new Room(1, 2, 102, 1));
+
+        Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
+        hotel.addManager("Santosh");
+        Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
+
+        Room rm = hotel.getRoom(sukhvindar.getDetails(), 3);
+        assertNotNull(rm);
+    }
 }
