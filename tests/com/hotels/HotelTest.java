f8f8cd7c9a0492641e8a8a5347460d8a9812395b
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-14 17:01:28 +0530

    v1.0 | new method totalRooms() is introduced. initiateRooms() is modified.

--- tests/com/hotels/HotelTest.java
+++ tests/com/hotels/HotelTest.java
@@ -1,10 +1,13 @@
 package com.hotels;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -12,29 +15,36 @@ import static org.junit.Assert.assertNull;
  * Created by kaustavc on 3/14/2015.
  * Reception test file for a hotel transaction
  */
-public class ReceptionTest {
-    @Test
-    public void testCustomerShouldGetRoomIfAvailable() {
-        List<Room> rooms = new ArrayList<Room>();
+public class HotelTest {
+    List<Room> rooms;
+    Hotel hotel;
+    Customer sukhvindar;
+
+    @Before
+    public void setUp() throws Exception {
+        rooms = new ArrayList<Room>();
         rooms.add(new Room(2, 3, 101, 2));
 
-        Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
+        hotel = new Hotel("Treejon Guest House", rooms, "Puri");
         hotel.addManager("Santosh");
-        Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
+        sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        rooms = new ArrayList<Room>();
+        hotel = null;
+        sukhvindar = null;
+    }
+
+    @Test
+    public void testCustomerShouldGetRoomIfAvailable() {
         Room rm = hotel.getRoom(sukhvindar.getDetails(), 3);
         assertNotNull(rm);
     }
 
     @Test
     public void testCustomerShouldGetRoomIfNotAvailable() {
-        List<Room> rooms = new ArrayList<Room>();
-        rooms.add(new Room(2, 3, 101, 2));
-
-        Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
-        hotel.addManager("Santosh");
-        Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
-
         Room rm = hotel.getRoom(sukhvindar.getDetails(), 4);
         assertNull(rm);
     }
@@ -46,11 +56,19 @@ public class ReceptionTest {
         rooms.add(new Room(2, 3, 202, 2));
         rooms.add(new Room(1, 2, 102, 1));
 
-        Hotel hotel = new Hotel("Treejon Guest House", rooms, "Puri");
-        hotel.addManager("Santosh");
-        Customer sukhvindar = new Customer("Sukhvindar", new City("Chandigarh"), 42, 1234);
-
         Room rm = hotel.getRoom(sukhvindar.getDetails(), 3);
         assertNotNull(rm);
+    }
+
+    @Test
+    public void testHotelShouldAddANewRoom() {
+        Room rm = new Room(3, 4, 301, 3);
+
+        hotel.addRoom(rm);
+        assertEquals(2, hotel.totalRooms());
+
+        new Room(3, 4, 301, 3);
+        hotel.addRoom(rm);
+        assertEquals(3, hotel.totalRooms());
     }
 }
